c5b1de70d234596701b30826694d4815b79edc46
asc1 <asc1@10.9.34.26>
2014-09-10 12:05:15 -0500

    First lab

---
+++ ACSWeiler/src/lab08/ForLoopDemo.java
@@ -0,0 +1,25 @@
+package lab08;
+
+//Name - Andrew Weiler
+//Date -
+//Class - Period 5
+//Lab  -
+
+import static java.lang.System.*;
+
+public class ForLoopDemo
+{
+	//instance variables and constructors could be used, but are not really needed
+	
+	//runForLoop() will run a for loop from start to stop going up by increment
+	public static void runForLoop(int start, int stop, int increment )
+	{
+		for(int i = start; i<stop; i+=increment)
+			System.out.print(i+" ");
+		System.out.println("");
+
+
+
+
+	}
+}
---
+++ ACSWeiler/src/lab08/Lab08b.java
@@ -0,0 +1,25 @@
+package lab08;
+
+//Name - Andrew Weiler
+//Date -
+//Class - Period 5
+//Lab  -
+
+public class Lab08b
+{
+	public static void main ( String[] args )
+	{
+		ForLoopDemo.runForLoop(2,90,5);
+		ForLoopDemo.runForLoop(3,76,4);
+		ForLoopDemo.runForLoop(-10,8,2);
+		ForLoopDemo.runForLoop(5,30,5);
+		ForLoopDemo.runForLoop(100,150,5);
+
+		//add more test cases
+		
+		
+		
+		
+		
+	}
+}
