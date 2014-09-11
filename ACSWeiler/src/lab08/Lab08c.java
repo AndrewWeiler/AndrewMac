3dc38454288a95128aa55e395e6101494e30c621
asc1 <asc1@10.9.34.26>
2014-09-11 12:24:06 -0500

    new commit

--- ACSWeiler/src/lab08/Lab08c.java
+++ ACSWeiler/src/lab08/Lab08c.java
@@ -11,7 +11,7 @@ public class Lab08c {
 	public static void main(String[] args) {
 		Box andrew = new Box("Andrew");
 		andrew.print();
-		Box natalie = new Box("Natalie");
+		Box natalie = new Box("Nataliy");
 		// add test cases
 		
 		natalie.print();
