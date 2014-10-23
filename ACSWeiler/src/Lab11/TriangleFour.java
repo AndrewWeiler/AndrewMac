fc8fe7d9e54ca492cca967be28ebea36302f0561
asc1 <asc1@10.9.34.26>
2014-10-23 12:25:01 -0500

    Finished lab11d use 11d for 11e

--- ACSWeiler/src/Lab11/TriangleFour.java
+++ ACSWeiler/src/Lab11/TriangleFour.java
@@ -6,26 +6,27 @@ package Lab11;
 //Lab  -
 
 import static java.lang.System.*;
-import static java.lang.System.*;
 
 
 
-	public class TriangleThree {
+
+	public class TriangleFour {
 
 		// this lab is setup with a single static method
 		// there are no instance variables or additional methods / constructors
 
-		public static String createTriangle3(String let, int size) {
+		public static String createTriangle4(String let, int size) {
 			String output = "";
 			//return output;
 			int count = 1;
 			for (int i = size; i > 0; i--) {
 				output += "\n";
-				for (int c = i; c > 0; c--)
-					output +=  " ";
-					for(int x = count; x>0; x--)
-						output += let;
-						count++;
+				for(int x = count; x>0; x--)
+					output += " ";
+					count++;
+					for (int c = i; c > 0; c--)
+					output +=  let;
+					
 					
 				}
 			
---
+++ ACSWeiler/src/Lab11/lab11d.java
@@ -0,0 +1,30 @@
+package Lab11;
+
+//Name - Andrew Weiler
+//Date -
+//Class - 
+//Lab  -
+import static java.lang.System.*;
+
+import java.util.Scanner;
+
+public class lab11d
+{
+ public static void main( String args[] )
+ {
+	 	Scanner keyboard = new Scanner(System.in);
+  	 	String choice="";
+  			do{
+  				out.print("Enter the size of the triangle : ");
+  				int big = keyboard.nextInt();
+  				out.print("Enter a letter : ");
+  				String value = keyboard.next();
+
+  					//static methods can be called using the class name
+  				System.out.println( TriangleFour.createTriangle4( value, big ) );
+
+  				System.out.print("Do you want to enter more data? ");
+  				choice=keyboard.next();
+  			}while(choice.equals("Y")||choice.equals("y"));
+  		}
+  	}
